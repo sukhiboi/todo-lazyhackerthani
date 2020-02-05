bc2de863ae5e31686e5ab49f175526fe8aedb732
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-02-05 12:01:00 +0530

    removed 2 unwanted function in client side script

--- src/public/scripts/indexScript.js
+++ src/public/scripts/indexScript.js
@@ -1,7 +1,3 @@
-const openForm = () => show('myForm');
-
-const closeForm = () => hide('myForm');
-
 const hide = id => (document.getElementById(id).style.display = 'none');
 
 const show = id => (document.getElementById(id).style.display = 'block');
