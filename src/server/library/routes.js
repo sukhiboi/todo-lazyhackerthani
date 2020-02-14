d2d4c7bd33afc77f48332fda6416bf48601ff33d
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-14 11:52:04 +0530

    [thanya|sukhiboi] Created routes.js

---
+++ src/server/library/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const TODO_STORE_PATH = require(`${__dirname}/../../../config.js`);
+const TodoStore = require('./todoStore');
+const handlers = require('./handlers');
+
+const app = express();
+app.use(express.static(`${__dirname}/../../public`));
+app.use(express.json());
+
+app.locals.store = new TodoStore(TODO_STORE_PATH);
+app.locals.store.initialize();
+
+app.get('/getTodos', handlers.getTodos);
+app.post('/createTodo', handlers.createTodo);
+app.post('/editTodoTitle', handlers.editTodoTitle);
+app.post('/deleteTodo', handlers.deleteTodo);
+app.post('/createTask', handlers.createTask);
+app.post('/editTaskCaption', handlers.editTaskCaption);
+app.post('/editTaskStatus', handlers.editTaskStatus);
+app.post('/deleteTask', handlers.deleteTask);
+
+module.exports = app;
--- src/server/server.js
+++ src/server/server.js
@@ -1,33 +1,6 @@
-const express = require('express');
-const TODO_STORE_PATH = require(`${__dirname}/../../config.js`);
-const TodoStore = require('./library/todoStore');
-const {
-  getTodos,
-  createTask,
-  createTodo,
-  deleteTask,
-  deleteTodo,
-  editTaskCaption,
-  editTaskStatus,
-  editTodoTitle
-} = require('./library/handlers');
+const app = require('./library/routes');
 
 const PORT = 7000;
-const app = express();
-app.use(express.static(`${__dirname}/../public`));
-app.use(express.json());
-
-app.locals.store = new TodoStore(TODO_STORE_PATH);
-app.locals.store.initialize();
-
-app.get('/getTodos', getTodos);
-app.post('/createTodo', createTodo);
-app.post('/editTodoTitle', editTodoTitle);
-app.post('/deleteTodo', deleteTodo);
-app.post('/createTask', createTask);
-app.post('/editTaskCaption', editTaskCaption);
-app.post('/editTaskStatus', editTaskStatus);
-app.post('/deleteTask', deleteTask);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
