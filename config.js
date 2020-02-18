6be839b7c3541f4796d0ccf2084ecdc46c4f6e3e
sukhiboi <41sukhdevsingh@gmail.com>
2020-02-18 10:20:51 +0530

    [thanya|sukhiboi] Creating DataStorage when the app starts

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const DATA_STORE_PATH =
   process.env.DATA_STORE_PATH || `${__dirname}/data/datastore.json`;
 const USERS_PATH = process.env.USERS_PATH || `${__dirname}/data/users.json`;
-module.exports = { DATA_STORE_PATH, USERS_PATH };
+DATA_STORAGE_PATH = process.env.DATA_STORE_PATH || `${__dirname}/data`;
+module.exports = { DATA_STORE_PATH, USERS_PATH, DATA_STORAGE_PATH };
